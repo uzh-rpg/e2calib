868e1759ccebbabfe41678087733534a87bd27b4
marcocannici <marco.cannici@mail.polimi.it>
2023-05-25 17:24:56 +0200

    Add option to extract rising, falling, or middle trigger timestamps

--- python/extract_triggers_prophesee.py
+++ python/extract_triggers_prophesee.py
@@ -21,19 +21,21 @@ def get_ext_trigger_timestamps(rawfile: Path):
     return {'t': time, 'p': pol}
 
 
-def get_reconstruction_timestamps(time: np.ndarray, pol: np.ndarray, use_avg_ts: bool=False, time_offset_us: int=0):
+def get_reconstruction_timestamps(time: np.ndarray, pol: np.ndarray, extract_ts: str = "rising", time_offset_us: int=0):
     assert 0 <= pol.max() <= 1
     assert np.all(np.abs(np.diff(pol)) == 1), 'polarity must alternate from trigger to trigger'
 
     timestamps = None
-    if use_avg_ts:
+    if extract_ts == "middle":
         assert pol[0] == 1, 'first ext trigger polarity must be positive'
         assert pol[-1] == 0, 'last ext trigger polarity must be negative'
         rising_ts = time[pol==1]
         falling_ts = time[pol==0]
         timestamps = ((rising_ts + falling_ts)/2).astype('int64')
-    else:
+    elif extract_ts == "rising":
         timestamps = time[pol==1]
+    elif extract_ts == "falling":
+        timestamps = time[pol==0]
 
     timestamps = timestamps + time_offset_us
     return timestamps
@@ -43,9 +45,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('Read trigger data from prophesee raw file')
     parser.add_argument('rawfile')
     parser.add_argument('output_file', help='Path to output text file with timestamps for reconstruction.')
-    parser.add_argument('--use_average_timestamp', '-avgt', action='store_true',
-            help='Take the average timestamp of rising and falling edge for the timestamp. '
-            'Otherwise, use the rising edge as timestamp.')
+    parser.add_argument('--use_timestamp', choices=["rising", "middle", "falling"], default="rising",
+            help='Whether to take the timestamp of the rising, falling, or the middle tiemstamp.')
     parser.add_argument('--time_offset_us', '-offset', type=int, default=0,
             help='Add a constant time offset to the timestamp for reconstruction. '
             'Can be useful to compensate for a constant exposure time of global shutter cameras.')
@@ -58,5 +59,5 @@ if __name__ == '__main__':
     assert outfile.suffix == '.txt'
 
     triggers = get_ext_trigger_timestamps(rawfile)
-    reconstruction_ts = get_reconstruction_timestamps(triggers['t'], triggers['p'], args.use_average_timestamp, args.time_offset_us)
+    reconstruction_ts = get_reconstruction_timestamps(triggers['t'], triggers['p'], args.use_timestamp, args.time_offset_us)
     np.savetxt(str(outfile), reconstruction_ts, '%i')
